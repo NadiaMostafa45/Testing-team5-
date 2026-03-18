3d45204ade8fdf67b82dc6f23b29152b6609ab26
nancyjosf <your_email@example.com>
2026-03-18 14:49:04 +0200

    Complete Lab 3

--- Task3.py
+++ Task3.py
@@ -1,7 +1,7 @@
 import unittest
 
 class User:
-    def __init__(self, name, email):
+    def __init__(self, name, email): 
         self._name = name
         self._email = email
 
@@ -14,21 +14,23 @@ class User:
         if new_email is not None:
             self._email = new_email
         return self._email
+
+class TestUser(unittest.TestCase):
     
-def setUp(self):
-    self.user=User("John Doe", "john.doe@example.com")
+    def setUp(self):
+        self.user = User("John Doe", "john.doe@example.com")
 
-def test_init(self):
-    self.assertEqual(self.user.name(),"John Doe")
-    self.assertEqual(self.user.email(),"john.doe@example.com")
+    def test_init(self):
+        self.assertEqual(self.user.name(), "John Doe")
+        self.assertEqual(self.user.email(), "john.doe@example.com")
 
-def update_name(self):
-    self.user.name("Nadia")
-    self.assertEqual(self.user.name(),"Nadia")
+    def test_update_name(self): 
+        self.user.name("Nadia")
+        self.assertEqual(self.user.name(), "Nadia")
 
-def update_email(self):
-    self.user.email("nadia.doe@example.com")
-    self.assertEqual(self.user.email(),"nadia.doe@example.com")
+    def test_update_email(self):
+        self.user.email("nadia.doe@example.com")
+        self.assertEqual(self.user.email(), "nadia.doe@example.com")
 
 if __name__ == '__main__':
     unittest.main()
